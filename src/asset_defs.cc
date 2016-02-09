933c52c389c723a0340952f51bae704e36f81a2d
Michal Vyskocil <michalvyskocil@eaton.com>
2016-02-09 22:59:27 +0100

    Problem: build fail in CI
    Solution: add the missing files to git
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

---
+++ src/asset_defs.cc
@@ -0,0 +1,119 @@
+/*  =========================================================================
+    asset_defs - Some fancy definitions related to assets
+
+    Copyright (C) 2014 - 2015 Eaton                                        
+                                                                           
+    This program is free software; you can redistribute it and/or modify   
+    it under the terms of the GNU General Public License as published by   
+    the Free Software Foundation; either version 2 of the License, or      
+    (at your option) any later version.                                    
+                                                                           
+    This program is distributed in the hope that it will be useful,        
+    but WITHOUT ANY WARRANTY; without even the implied warranty of         
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+    GNU General Public License for more details.                           
+                                                                           
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    =========================================================================
+*/
+
+/*
+@header
+    asset_defs - Some fancy definitions related to assets
+@discuss
+@end
+*/
+
+#include "agent_asset_classes.h"
+
+//  Structure of our class
+
+//  translate numeric ID to const char*
+const char *asset_type2str (int asset_type)
+{
+    switch (asset_type) {
+        case GROUP:
+            return "group";
+        case DATACENTER:
+            return "datacenter";
+        case ROOM:
+            return "room";
+        case ROW:
+            return "row";
+        case RACK:
+            return "rack";
+        case DEVICE:
+            return "device";
+        default:
+            return "unknown";
+    };
+    return "make-gcc-happy"; // should not reach here
+}
+
+//  translate numeric ID to const char*
+const char *asset_subtype2str (int asset_subtype)
+{
+    switch (asset_subtype) {
+        case UPS:
+            return "ups";
+        case GENSET:
+            return "genset";
+        case EPDU:
+            return "epdu";
+        case PDU:
+            return "pdu";
+        case SERVER:
+            return "server";
+        case FEED:
+            return "feed";
+        case STS:
+            return "sts";
+        case SWITCH:
+            return "switch";
+        case STORAGE:
+            return "storage";
+        case VIRTUAL:
+            return "virtual";
+        default:
+            return "unknown";
+    };
+    return "make-gcc-happy"; // should not reach here
+}
+
+//  translate numeric ID to const char*
+const char *asset_op2str (int asset_op)
+{
+
+    switch (asset_op)
+    {
+        case INSERT:
+            return "create";
+        case DELETE:
+            return "delete";
+        case UPDATE:
+            return "update";
+        case RETIRE:
+            return "retire";
+        default:
+            return "unknown";
+    }
+    return "make-gcc-happy"; // should not reach here
+}
+
+//  --------------------------------------------------------------------------
+//  Self test of this class
+
+void
+asset_defs_test (bool verbose)
+{
+    printf (" * asset_defs: ");
+
+    //  @selftest
+    assert (streq (asset_type2str (2), "datacenter"));
+    assert (streq (asset_subtype2str (2), "genset"));
+    assert (streq (asset_op2str (2), "delete"));
+    //  @end
+    printf ("OK\n");
+}
---
+++ src/asset_defs.h
@@ -0,0 +1,81 @@
+/*  =========================================================================
+    asset_defs - Some fancy definitions related to assets
+
+    Copyright (C) 2014 - 2015 Eaton                                        
+                                                                           
+    This program is free software; you can redistribute it and/or modify   
+    it under the terms of the GNU General Public License as published by   
+    the Free Software Foundation; either version 2 of the License, or      
+    (at your option) any later version.                                    
+                                                                           
+    This program is distributed in the hope that it will be useful,        
+    but WITHOUT ANY WARRANTY; without even the implied warranty of         
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+    GNU General Public License for more details.                           
+                                                                           
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    =========================================================================
+*/
+
+#ifndef ASSET_DEFS_H_INCLUDED
+#define ASSET_DEFS_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//  @interface
+
+// Enum to define legacy asset type and subtype
+enum asset_type {
+    TUNKNOWN     = 0,
+    GROUP       = 1,
+    DATACENTER  = 2,
+    ROOM        = 3,
+    ROW         = 4,
+    RACK        = 5,
+    DEVICE      = 6
+};
+
+enum asset_subtype {
+    SUNKNOWN = 0,
+    UPS = 1,
+    GENSET,
+    EPDU,
+    PDU,
+    SERVER,
+    FEED,
+    STS,
+    SWITCH,
+    STORAGE,
+    VIRTUAL,
+    N_A = 11
+    /* ATTENTION: don't change N_A id. It is used as default value in init.sql for types, that don't have N_A */
+};
+
+enum asset_operation
+{
+    INSERT = 1,
+    DELETE,
+    UPDATE,
+    GET,
+    RETIRE
+};
+
+//  translate numeric ID to const char*
+const char *asset_type2str (int asset_type);
+
+//  translate numeric ID to const char*
+const char *asset_subtype2str (int asset_subtype);
+
+//  translate numeric ID to const char*
+const char *asset_op2str (int asset_op);
+//  @end
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
