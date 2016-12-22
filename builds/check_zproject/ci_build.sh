456d0e4d893736cec5b6e949f9efc4fc8bb0b28a
Karol Hrdina <KarolHrdina@Eaton.com>
2016-12-22 10:01:36 +0100

    PROBLEM: building core needs libmagic
    SOLUTION: change use element order; regen
    
    Signed-off-by: Karol Hrdina <KarolHrdina@Eaton.com>

--- builds/check_zproject/ci_build.sh
+++ builds/check_zproject/ci_build.sh
@@ -5,10 +5,10 @@ cd $REPO_DIR/..
 git clone --quiet --depth 1 https://github.com/zeromq/libzmq.git libzmq.git
 git clone --quiet --depth 1 https://github.com/zeromq/czmq.git czmq.git
 git clone --quiet --depth 1 https://github.com/zeromq/malamute.git malamute.git
+git clone --quiet --depth 1 https://github.com/42ity/libmagic magic.git
 git clone --quiet --depth 1 https://github.com/42ity/fty-core core.git
 git clone --quiet --depth 1 https://github.com/42ity/fty-proto fty-proto.git
 git clone --quiet --depth 1 https://github.com/42ity/cxxtools cxxtools.git
-git clone --quiet --depth 1 https://github.com/42ity/libmagic magic.git
 cd -
 
 cd $REPO_DIR/..
