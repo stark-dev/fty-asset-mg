57ef63c82ca8aff91e929905207480af83306101
Jim Klimov <jim@jimklimov.com>
2020-04-23 10:42:36 +0200

    Bump copyrights to 2020

--- src/topology/persist/persist_error.h
+++ src/topology/persist/persist_error.h
@@ -1,7 +1,7 @@
 /*  =========================================================================
     topology_power_persist_persist_error - class description
 
-    Copyright (C) 2014 - 2018 Eaton
+    Copyright (C) 2014 - 2020 Eaton
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
